7afde6078a5cddcbf300c7aaf34c244919e15e05
Jovell Fernandes <jovell.fernandes@capgemini.com>
2020-02-17 12:31:00 +0530

    dictionary CLI

--- Dictionary/main.py
+++ Dictionary/main.py
@@ -1,14 +1,37 @@
 import json
 
 data = json.load(open("data.json"))
-words = str(input("Enter Word: "))
-flag=0
-for k in data:
-    if(words==k):
-        for i in data[k]:
-            print("\n"+i)
-        print("\n")
-        flag=1
-        break
-if(flag==0):
-    print("\nCould not loacate word. Kindly check your spelling.")  
+x_it=0
+
+while(x_it==0):
+    print("\n\n\t\t\t   WELCOME TO WEBSTER'S DICTIONARY\n\n")
+    words = str(input("Enter Word: "))
+    flag=0
+    for k in data:
+        if(words==k):
+            for i in data[k]:
+                print("\n>>"+i)
+            print("\n")
+            flag=1
+            break
+    if(flag==0):
+        print("\nCould not loacate word. Kindly check your spelling.")
+    check_flag=0
+    while(check_flag==0):
+        re_enter=input("Check another word? (Y/N): ")
+        if(re_enter=='y'):
+            check_flag=1
+            x_it=0
+        elif(re_enter=='Y'):
+            check_flag=1
+            x_it=0
+        elif(re_enter=='n'):
+            check_flag=1
+            x_it=1
+        elif(re_enter=='N'):
+            check_flag=1
+            x_it=1
+        else:
+            print("\nWrong input. Please enter again.\n")
+            check_flag=0
+
